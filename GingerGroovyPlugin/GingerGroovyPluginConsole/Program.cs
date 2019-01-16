a8958a77f19d57c06d5c15f4c578816fc190e3f0
Sudarshan Sathe <sudarshan.sathe@amdocs.com>
2019-01-16 18:47:28 +0530

    setting up .NET Core project

---
+++ GingerGroovyPlugin/GingerGroovyPluginConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GingerGroovyPluginConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
